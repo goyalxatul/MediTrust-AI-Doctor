8782eb7d431786fe96bc46a3626f3eb67be892b7
atul goyal <atulxgoyal@gmail.com>
2025-10-26 20:09:53 +0530

    dunit3

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 set -o errexit  # Exit on first error
-apt-get update && apt-get install -y portaudio19-dev
+
+# Update package list and install system dependencies for PyAudio
+apt-get update
+apt-get install -y portaudio19-dev python3-dev
+
+# Upgrade pip to latest version
+pip install --upgrade pip
+
+# Install Python dependencies
 pip install -r requirements.txt
